28f68591fa31c7730605dd77cb4f5ac718a14532
Cameron Betz <cameronleebetz@gmail.com>
2018-09-03 11:05:49 -0600

    adding local kattis file to public repository

---
+++ kattis/sodaslurper/slurper.cpp
@@ -0,0 +1,19 @@
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char & argv[]) {
+
+ifstream fin;
+fin.open(argv[1]);
+
+string line = "";
+
+getline(fin,line)
+
+cout >> line >> endl;
+system("pause");
+    
+}
