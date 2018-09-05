c56a7670e8ba856ce9a44ac59265d5ce5ee965c8
Cameron Betz <cameronleebetz@gmail.com>
2018-09-05 11:05:45 -0600

    pushed kattis problem to slurper folder

--- kattis/sodaslurper/slurper.cpp
+++ kattis/sodaslurper/slurper.cpp
@@ -1,19 +1,53 @@
 #include <iomanip>
-#include <fstream>
+#include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-int main(int argc, char & argv[]) {
+int main(int argc, char* argv[]) {
 
-ifstream fin;
-fin.open(argv[1]);
+int e, f, c = 0;
+int list[3];
 
-string line = "";
-
-getline(fin,line)
-
-cout >> line >> endl;
-system("pause");
-    
+if(argc!=4){
+    cout << "Not correct amount of inputs given" << endl;
 }
+else{
+
+    for(int i = 0; i<=2; i++){
+    
+        list[i] = int(*argv[i+1]-48);
+    }
+    //# of soda bottles in tim's possesion
+    int e = list[0];
+    //# of bottles found
+    int f = list[1];
+    //number of bottles required to buy a new soda
+    int c = list[2];
+    int total = 0;
+    int empty = e+f;
+    if(e >= 1000 || f >= 1000 || c <2 || c >= 2000 || e <0 || f <0){
+        cout << "parameters out of bounds! " << endl;
+    }
+    else{
+    //logic here
+    
+    while(empty>=c){
+        empty -= c;
+        ++total;
+        ++empty;
+
+
+        }
+    }
+
+    cout << total << endl;
+
+
+    //----------
+    }
+    return 0;
+}
+
+
